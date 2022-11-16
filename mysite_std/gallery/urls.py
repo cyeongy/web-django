4dccf1fa500dbdb93c36a727ec4d4d7339465a0a
cyeongy <cyeongy@gmail.com>
2022-11-16 14:14:51 +0900

    Update: static file lecture

--- mysite_std/gallery/urls.py
+++ mysite_std/gallery/urls.py
@@ -4,8 +4,8 @@ from .models import Image
 
 app_name = 'gallery'
 urlpatterns = [
-    # path('', TemplateView.as_view(template_name='gallery/gallery_list.html'), name='list'),
-    # path('list/', ListView.as_view(model=Image), name='image_list'),
-    # path('detail/<pk>/', DetailView.as_view(model=Image), name='image_detail'),
-    # path('add/', CreateView.as_view(model=Image, fields='__all__'), name='image_add'),
+    path('', TemplateView.as_view(template_name='gallery/gallery_list.html'), name='list'),
+    path('list/', ListView.as_view(model=Image), name='image_list'),
+    path('detail/<pk>/', DetailView.as_view(model=Image), name='image_detail'),
+    path('add/', CreateView.as_view(model=Image, fields='__all__'), name='image_add'),
 ]
