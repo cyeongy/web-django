331f6c682ba6c26d643c8a8d53e689d80048b4ad
cyeongy <cyeongy@gmail.com>
2022-11-16 14:55:42 +0900

    Update: media path 설정하기

--- mysite_std/gallery/models.py
+++ mysite_std/gallery/models.py
@@ -3,7 +3,7 @@ from django.urls import reverse
 
 class Image(models.Model):
     title = models.CharField(max_length=200)
-    photo = models.ImageField()
+    photo = models.ImageField(upload_to='gallery/%Y/%m/%d/')
 
     def __str__(self):
         return self.title
