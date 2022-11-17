1a9d85e6c745b8626e27b6565fd5afc9be9d637c
cyeongy <cyeongy@gmail.com>
2022-11-17 16:52:06 +0900

    Update: Rest API 사용법

--- myproject2_restframework/api2/serializers.py
+++ myproject2_restframework/api2/serializers.py
@@ -1,9 +1,35 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
-from blog.models import *
-
+from blog import models
+# from ..blog.models import *
 # Serializers define the API representation.
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ['url', 'username', 'email', 'is_staff']
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Post
+        fields = '__all__'
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Comment
+        fields = '__all__'
+
+class PostListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Post
+        fields = ['id', 'title', 'image', 'like', 'category']
+
+class PostRetrieveSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Post
+        exclude = ['create_dt']
+
+class PostLikeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Post
+        fields = ['like']
--- myproject2_restframework/api2/urls.py
+++ myproject2_restframework/api2/urls.py
@@ -1,14 +1,27 @@
 from django.urls import path, include
 from rest_framework import routers
-from api2.views import *
+from api2 import views
 
 # Routers provide an easy way of automatically determining the URL conf.
 
-router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
+
+# # Router와 ViewSet을 등록하는 방법
+# router = routers.DefaultRouter()
+# router.register(r'users', views.UserViewSet)
+# router.register(r'post', views.PostViewSet)
+# router.register(r'comment', views.CommentViewSet)
+#
+# urlpatterns = [
+#     path('', include(router.urls)),
+# ]
+
+
 urlpatterns = [
-    path('', include(router.urls)),   
+    path('post/', views.PostListView.as_view(), name='post-list'),
+    path('post/<int:pk>/', views.PostRetrieveView.as_view(), name='post-detail'),
+    path('comment/', views.CommentView.as_view(), name='comment-list'),
+    path('post/<int:pk>/like/', views.PostLikeAPIView.as_view(), name='post-like')
 ]
